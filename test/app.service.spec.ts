7479dc15d7f974c42052b03a3ac38ab0e887aac3
hissmirk <false9@kakao.com>
2025-06-20 16:48:39 +0900

    Feat: Add unit tests and integrate into CI pipeline

--- test/app.service.spec.ts
+++ test/app.service.spec.ts
@@ -15,7 +15,7 @@ describe('AppService', () => {
   describe('getFormattedHello', () => {
     it('이름이 주어지면 "Hello, [이름]!"을 반환해야 한다', () => {
       const name = 'Juyeop';
-      const expectedResult = `Hello, ${name}!!`;
+      const expectedResult = `Hello, ${name}!`;
       // appService.getFormattedHello('Juyeop')의 결과가
       // 'Hello, Juyeop!'과 같은지(toBe) 확인(expect)합니다.
       expect(appService.getFormattedHello(name)).toBe(expectedResult);
